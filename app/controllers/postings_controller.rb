d517f8e6eac1ccd53086d4f26f36d6dc21f9e839
Rio <rnose512@gmail.com>
2017-09-16 20:34:54 -0700

    Fix syntax error in create method ;

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -5,13 +5,22 @@ class PostingsController < ApplicationController
   end
 
   def create
-    @posting = Posting.new(name: params[:name], contact: params[:latitude], latitude: params[:latitude], longitude: [:longitude],categories: params[:categories], number_of_people: params[:numberOfPeople])
+    @posting = Posting.new(
+      name: params[:name], 
+      contact: params[:contact], 
+      latitude: params[:latitude], 
+      longitude: [:longitude],
+      categories: params[:categories], 
+      number_of_people: params[:numberOfPeople]
+    )
 
     if @posting.save
       @posting
       render json: @posting
     else
-      render :json, { errors: ["Sign up failed!"], status: 422 }
+      render :json, 
+      { errors: ["Sign up failed!"], 
+        status: 422 }
     end
   end
 end
