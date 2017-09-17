557e41f0b4c94555c12701901d5a0ae94a6d2d4d
Rio <rnose512@gmail.com>
2017-09-16 21:28:44 -0700

    Work on form

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -15,7 +15,7 @@ class PostingsController < ApplicationController
     )
     coords = Geocoder.coordinates(params[:address])
     p params
-    @posting = Posting.new(name: params[:name], contact: params[:latitude], latitude: coords[0], longitude: coords[1],categories: params[:categories], number_of_people: params[:number_of_people])
+    @posting = Posting.new(name: params[:name], contact: params[:contact], latitude: coords[0], longitude: coords[1],categories: params[:categories], number_of_people: params[:number_of_people])
     p coords
     p "butts" * 100
 
